47c8639b20948f008fb25d30bc6f82786de0d25e
atiedebee <atiedebee@gmail.com>
2023-04-25 16:40:26 +0200

    added licence and readme

---
+++ README.md
@@ -0,0 +1,48 @@
+qoi implementation in C. Optimized using gcc vector extensions
+
+### Benchmarks
+
+|System Used |                                  |
+|------------|----------------------------------|
+|CPU         | Ryzen 7 pro 6850U                |
+|RAM         | 16GB DDR5 6400 MT/s              |
+|OS          | OpenSUSE TW 2022 12 02           |
+|GCC         | gcc (SUSE Linux) 12.2.1 20221020 |
+
+RGBA done with https://animecorner.me/wp-content/uploads/2023/03/Frieren.png
+The file size used to calculate the MB/s is the size of the RAW data (7.9MB)
+Each benchmark is done by running the compression part in a loop 100 times.
+
+
+compress (RGBA):
+
+|  | -O2               |      | -O3               |      |
+|--|-------------------|------|-------------------|------|
+|  | Time Average (ms) | MB/s | Time Average (ms) | MB/s |
+|--|-------------------|------|-------------------|------|
+|1 | 13.80             |  572 | 10.70             |  738 |
+|2 |  9.26             |  853 |  9.02             |  875 |
+|3 |  9.05             |  872 |  8.32             |  949 |
+|4 |  7.67             | 1029 |  7.80             | 1012 |
+
+
+1. base
+2. replaced __builtin_add_overflow in calculate_diff with vector addition and subtraction
+3. changed calculate_luma to partially use vectors
+4. replaced comparisons with integer comparison in union (for some reason also reduced filesize from 2.6 MB to 1.8 MB)
+
+
+
+decompress (RGBA):
+
+|  | -O2               |      | -O3               |      |
+|--|-------------------|------|-------------------|------|
+|  | Time Average (ms) | MB/s | Time Average (ms) | MB/s |
+|--|-------------------|------|-------------------|------|
+|1 |                   |      | 10.20             |  774 |
+|2 |  9.67             |  816 |  9.26             |  853 |
+|3 |  6.54             | 1207 |  6.21             | 1272 |
+
+1. base
+2. Removed pixelcounter from if else statements. And only put it in the run length decode. Add 1 to it at the end of the loop
+3. Replaced 'channels' variable with a constant (4) because I'll split the function up in an RGB and RGBA one anyways
--- qoi.c
+++ qoi.c
@@ -1,3 +1,22 @@
+/*
+qoi-c: a "fast" C implementation of the qoi format
+Copyright (C) 2023  atiedebee
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #include <endian.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -9,53 +28,6 @@
 
 typedef unsigned char ubyte;
 typedef unsigned char vec4u8 __attribute__((vector_size(4)));
-
-/*
-benchmarks
-
-== System Used ==
-CPU         Ryzen 7 pro 6850U
-RAM         16GB DDR5 6400 MT/s
-OS          OpenSUSE TW 2022 12 02
-GCC         gcc (SUSE Linux) 12.2.1 20221020
-
-RGBA done with https://animecorner.me/wp-content/uploads/2023/03/Frieren.png
-The file size used to calculate the MB/s is the size of the RAW data (7.9MB)
-Each benchmark is done by running the compression part in a loop 100 times.
-
-
-compress (RGBA):
-  | -O2                      | -O3
---+-------------------+------+-------------------+------+
-  | Time Average (ms) | MB/s | Time Average (ms) | MB/s |
---+-------------------+------+-------------------+------+
-1 | 13.80             |  572 | 10.70             |  738 |
-2 |  9.26             |  853 |  9.02             |  875 |
-3 |  9.05             |  872 |  8.32             |  949 |
-4 |  7.67             | 1029 |  7.80             | 1012 |
-
-
-1. base
-2. replaced __builtin_add_overflow in calculate_diff with vector addition and subtraction
-3. changed calculate_luma to partially use vectors
-4. replaced comparisons with integer comparison in union (for some reason also reduced filesize from 2.6 MB to 1.8 MB)
-
-
-
-decompress (RGBA):
-  | -O2                      | -O3
---+-------------------+------+-------------------+------+
-  | Time Average (ms) | MB/s | Time Average (ms) | MB/s |
---+-------------------+------+-------------------+------+
-1 |                   |      | 10.20             |  774 |
-2 |  9.67             |  816 |  9.26             |  853 |
-3 |  6.54             | 1207 |  6.21             | 1272 |
-
-1. base
-2. Removed pixelcounter from if else statements. And only put it in the run length decode. Add 1 to it at the end of the loop
-3. Replaced 'channels' variable with a constant (4) because I'll split the function up in an RGB and RGBA one anyways
-*/
-
 
 union Pixel{
     struct{
